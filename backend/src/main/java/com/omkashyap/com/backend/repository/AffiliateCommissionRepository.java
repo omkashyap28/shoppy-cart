e4b8556722703a94a577554540af573447cce77d
Hariom Kashyap <omkashyap6870@gmail.com>
2026-05-21 23:09:48 +0530

    implementing affiliate services

--- backend/src/main/java/com/omkashyap/com/backend/repository/AffiliateCommissionRepository.java
+++ backend/src/main/java/com/omkashyap/com/backend/repository/AffiliateCommissionRepository.java
@@ -1,11 +1,9 @@
 package com.omkashyap.com.backend.repository;
 
 import com.omkashyap.com.backend.entity.AffiliateCommission;
-import com.omkashyap.com.backend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-import java.util.OptionalDouble;
 
 public interface AffiliateCommissionRepository extends JpaRepository<AffiliateCommission, Long> {
   Optional<AffiliateCommission> findByCategoryName(String name);
