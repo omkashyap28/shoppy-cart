48c074f6b9aa02ac4ea47842086a33024559465e
Hariom Kashyap <omkashyap6870@gmail.com>
2026-06-04 19:56:09 +0530

    creating webpage footer layout

--- frontend/components/layout/footer.tsx
+++ frontend/components/layout/footer.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import Link from "next/link";
 import { Button } from "../ui/button";
 import { Logo } from "./logo";
+import {
+  footerLinksSection1,
+  footerLinksSection2,
+  footerLinksSection3,
+  footerLinksSection4,
+} from "@/constants";
+import React from "react";
 
 export function Footer() {
   const handleClick = () => {
@@ -20,15 +28,84 @@ export function Footer() {
       <div className="w-full">
         <Button
           onClick={handleClick}
-          className="h-10 w-full bg-transparent text-foreground hover:bg-input"
+          className="mb-7 h-12 w-full rounded-none bg-input text-foreground hover:bg-transparent"
         >
           Go to Top
         </Button>
       </div>
       <Logo />
-      <div className="w-full text-center text-sm font-light tracking-tight">
-        &copy; All rights reserved for Shoppy Cart
+      <div className="py-8 md:px-5">
+        <div className="grid font-sans text-sm font-normal sm:grid-cols-2 md:grid-cols-4">
+          <div>
+            <h4 className="mt-4 mb-2 text-[15px] font-semibold tracking-tight">
+              Categories
+            </h4>
+            <ul className="flex flex-col gap-1.5">
+              {footerLinksSection1.map(({ label, href }) => (
+                <li key={label}>
+                  <LinkItem href={href}>{label}</LinkItem>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h4 className="mt-4 mb-2 text-[15px] font-semibold tracking-tight">
+              User Stuff
+            </h4>
+            <ul className="flex flex-col gap-1.5">
+              {footerLinksSection2.map(({ label, href }) => (
+                <li key={label}>
+                  <LinkItem href={href}>{label}</LinkItem>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h4 className="mt-4 mb-2 text-[15px] font-semibold tracking-tight">
+              Earnings
+            </h4>
+            <ul className="flex flex-col gap-1.5">
+              {footerLinksSection3.map(({ label, href }) => (
+                <li key={label}>
+                  <LinkItem href={href}>{label}</LinkItem>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h4 className="mt-4 mb-2 text-[15px] font-semibold tracking-tight">
+              Also Check
+            </h4>
+            <ul className="flex flex-col gap-1.5">
+              {footerLinksSection4.map(({ label, href }) => (
+                <li key={label}>
+                  <LinkItem href={href}>{label}</LinkItem>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="mt-7 w-full border-t border-muted py-2 text-center text-sm font-light tracking-tight">
+          &copy; All rights reserved for Shoppy Cart
+        </div>
       </div>
     </footer>
+  );
+}
+
+function LinkItem({
+  children,
+  href,
+}: {
+  children: React.ReactNode;
+  href: string;
+}) {
+  return (
+    <Link
+      href={href}
+      className="underline-offset-1 transition-all duration-100 hover:underline max-sm:text-center"
+    >
+      {children}
+    </Link>
   );
 }
--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -26,3 +26,103 @@ export const navLinks = [
     label: "Contact",
   },
 ];
+
+export const footerLinksSection1 = [
+  {
+    href: "#",
+    label: "Electronics",
+  },
+  {
+    href: "#",
+    label: "Fashion",
+  },
+  {
+    href: "#",
+    label: "Groceries",
+  },
+  {
+    href: "#",
+    label: "Kitchen",
+  },
+  {
+    href: "#",
+    label: "Home Decors",
+  },
+  {
+    href: "#",
+    label: "Cars Accessories",
+  },
+  {
+    href: "#",
+    label: "Bike Accessories",
+  },
+];
+
+export const footerLinksSection2 = [
+  {
+    href: "#",
+    label: "Cart Items",
+  },
+  {
+    href: "#",
+    label: "Orders",
+  },
+  {
+    href: "#",
+    label: "Returns",
+  },
+  {
+    href: "#",
+    label: "Wallet",
+  },
+  {
+    href: "#",
+    label: "Recent Viewed",
+  },
+  {
+    href: "#",
+    label: "Track Order",
+  },
+];
+
+export const footerLinksSection3 = [
+  {
+    href: "#",
+    label: "Seller",
+  },
+  {
+    href: "#",
+    label: "Affiliate",
+  },
+  {
+    href: "#",
+    label: "Delivery Partner",
+  },
+  {
+    href: "#",
+    label: "Sponser",
+  },
+];
+
+export const footerLinksSection4 = [
+  {
+    href: "#",
+    label: "Trendings",
+  },
+  {
+    href: "#",
+    label: "Expore Products",
+  },
+  {
+    href: "#",
+    label: "Best Sellers",
+  },
+  {
+    href: "#",
+    label: "Sale",
+  },
+  {
+    href: "#",
+    label: "Best Selling Products",
+  },
+];
