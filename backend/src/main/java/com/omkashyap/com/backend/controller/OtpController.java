8a1c5a9e8a5d4921067927c8a40841f5f21edcc3
Hariom Kashyap <omkashyap6870@gmail.com>
2026-06-14 17:11:31 +0530

    setting otpVerified cookie when otp is verified

--- backend/src/main/java/com/omkashyap/com/backend/controller/OtpController.java
+++ backend/src/main/java/com/omkashyap/com/backend/controller/OtpController.java
@@ -3,10 +3,13 @@ package com.omkashyap.com.backend.controller;
 import com.omkashyap.com.backend.dto.requestDto.OtpRequestDto;
 import com.omkashyap.com.backend.dto.responseDto.OtpResponseDto;
 import com.omkashyap.com.backend.service.OtpService;
+
+import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,7 +27,15 @@ public class OtpController {
   @PostMapping("/verify")
   public ResponseEntity<OtpResponseDto> verifyOtp(
       @RequestHeader("Authorization") String authHeader,
-      @RequestBody OtpRequestDto requestDto) {
+      @RequestBody OtpRequestDto requestDto,
+      HttpServletResponse response) {
+
+    Cookie cookie = new Cookie("otpVerified", "true");
+    cookie.setMaxAge(60 * 60 * 24 * 7);
+    cookie.setHttpOnly(true);
+    cookie.setSecure(false);
+    cookie.setPath("/");
+    response.addCookie(cookie);
 
     return ResponseEntity.ok(otpService.verifyOtp(authHeader, requestDto));
   }
