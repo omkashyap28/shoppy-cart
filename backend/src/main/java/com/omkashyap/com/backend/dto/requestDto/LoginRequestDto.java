4092bfb7b1f816a1bd6fd9f7b8f105621bf641f5
Hariom Kashyap <omkashyap6870@gmail.com>
2026-06-24 21:50:45 +0530

    fir typo in LoginRequestDto

--- backend/src/main/java/com/omkashyap/com/backend/dto/requestDto/LoginRequestDto.java
+++ backend/src/main/java/com/omkashyap/com/backend/dto/requestDto/LoginRequestDto.java
@@ -15,6 +15,6 @@ public class LoginRequestDto {
   private String email;
 
   @NotBlank(message = "Password is required")
-  @Size(min = 10, max = 50, message = "Password must be at least 6 characters")
+  @Size(min = 10, max = 50, message = "Password must be at least 10 characters")
   private String password;
 }
