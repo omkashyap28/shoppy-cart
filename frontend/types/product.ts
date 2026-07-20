13be550e1d79f832281026079cc03b0b4ef7c4fa
Hariom Kashyap <omkashyap6870@gmail.com>
2026-07-20 22:16:22 +0530

    feat: define ProductImage, ProductCardProps, and Product interfaces for product management

--- frontend/types/product.ts
+++ frontend/types/product.ts
@@ -6,3 +6,45 @@ export interface UploadedItems {
   priority: number;
   altText: string;
 }
+
+export interface ProductImage {
+  altText: string | null;
+  imageId: string;
+  imageUrl: string;
+  priority: number;
+  thumbnailUrl: string;
+}
+
+export interface ProductCardProps {
+  productId: string;
+  brandName: string;
+  description: string;
+  productThumbnail: string;
+  sellerId: string;
+  inStock: boolean;
+  totalReviews: number;
+  averageRating: number;
+  categoryId: number;
+  price: number;
+  coins: number;
+  productUrl: string;
+  tags: string[];
+}
+
+export interface Product {
+  averageRating: string;
+  brandName: string;
+  categoryId: number;
+  coins: number;
+  description: string;
+  inStock: boolean;
+  price: number;
+  productAttributes: Map<string, string>;
+  productId: string;
+  productImages: ProductImage[];
+  productThumbnail: string;
+  productUrl: string;
+  sellerId: string;
+  tags: string[];
+  totalReviews: number;
+}
