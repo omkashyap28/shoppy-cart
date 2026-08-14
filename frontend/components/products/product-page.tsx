18cc9ddb324b9f2af88423fd030d71aa0e2c7409
Hariom Kashyap <omkashyap6870@gmail.com>
2026-08-14 22:32:35 +0530

    feat: add initial product page, types, loading component and error component

---
+++ frontend/components/products/product-page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Products } from "@/types/product";
+import { InfiniteScroll } from "../layout/infinite-scroll";
+import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
+import { ProductCard } from "./product-card";
+
+export function ProductPage() {
+  const {
+    response,
+    fetchNextPage,
+    isFetchNextPageError,
+    isFetchingNextPage,
+    hasNextPage,
+    refetch,
+    status,
+  } = useInfiniteScroll<Products>({
+    queryKey: [`products`],
+    queryFn: async () => {
+      const response = await fetch(`/backend/search/initial`);
+      
+      if(response.status !== 200) {
+        throw new Error("Failed to get initial product");
+      }
+
+      return await response.json();
+    },
+  });
+
+  return (
+    <InfiniteScroll
+      isError={status === "error"}
+      hasMore={hasNextPage}
+      loading={status === "pending"}
+      onLoadMore={fetchNextPage}
+      onRetry={refetch}
+      isFetchNextPageError={isFetchNextPageError}
+      isFetchingNextPage={isFetchingNextPage}
+    >
+      <section className="grid grid-cols-2 gap-4 md:gap-6 py-4 max-xs:p-4 sm:grid-cols-3 md:p-6 lg:grid-cols-4">
+        {response?.map((product) => (
+          <ProductCard product={product} key={product.productId} />
+        ))}
+      </section>
+    </InfiniteScroll>
+  );
+}
